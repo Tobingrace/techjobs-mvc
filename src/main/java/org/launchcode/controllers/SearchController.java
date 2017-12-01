2489e2e7b6dfc0f4bc1aae70f130f9dd9c101582
Tobingrace <jasonhart179@gmail.com>
2017-12-01 09:55:25 -0600

    Completed all TODO's

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -23,5 +24,26 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String search(@RequestParam String searchType, @RequestParam String searchTerm, Model model) {
 
+        //Creating iterable hashmap to hold returned value and pass to the page
+        Iterable<HashMap<String, String>> jobs;
+
+        //Search by all eliminates the need to use searchType
+        if (searchType.equals("all")) {
+            jobs = JobData.findByValue(searchTerm);
+        }
+        //Otherwise default to findByColumnAndValue
+        else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("selectedColumn", searchType);
+        }
+
+        //Adding the attributes to the model so Thymeleaf can access them in the view
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
 }
